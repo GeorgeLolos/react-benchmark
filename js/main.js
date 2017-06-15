2eb06dde63502a2c7e75268c6a3abdc18bdec1e8
georgios.lolos <lolos_george@hotmail.com>
2017-06-15 14:47:31 +0200

    beautification

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 /**
- * @description stateless component
+ * @description stateless functional component
  * @param {Object} props
  * @constructor Dot
  */
@@ -10,7 +10,7 @@ const Dot = (props) =>
   <span>{props.hi}</span>;
 
 /**
- * @description Statefull Component
+ * @description Stateful Component
  */
 class DotComponent extends React.Component {
   render() {
@@ -23,27 +23,33 @@ class DotComponent extends React.Component {
  */
 class Main extends React.Component {
   render() {
-    const dots = new Array(500).fill(0).map(x => {
-      if(this.props.kind == 'stateless-functional-direct-call') {
+    //create an array of 500 elements and fill them with zeros
+    //Map this array with a function that calls each component in it's way
+    const hi = new Array(500).fill(0).map(x => {
+      if(this.props.kind == 'functionalComponentAsFunction') {
+        //call the functional component as a function
         return Dot({hi: 'hi'});
-      } else if(this.props.kind == 'stateless-functional-mounted') {
+      } else if(this.props.kind == 'functionalComponentAsReactElement') {
+        //call the functional component as a React Element
         return <Dot hi="hi"/>;
-      } else if(this.props.kind == 'stateful') {
+      } else if(this.props.kind == 'classicalComponent') {
+        //call the Class created component as a React Element
         return <DotComponent hi="hi"/>
       }
     });
-    return React.createElement('div', {}, ...dots);
+    //create a div and ouput the created elements
+    return React.createElement('div', {}, ...hi);
   }
 }
 
 //Benchmarking variables
-let benchmarkCount, statefulTotalTime, statelessFunctionalMountedTotalTime, statelessFunctionalDirectCallTotalTime;
+let benchmarkCount, classicalComponentTotalTime, functionalComponentAsElement, functionallyCalledComponent;
 /**
  * @description Run is called as many times as the benchmark defines. Internally it calls 500 times every component for each render.
  * It calculates in total how much time it took to render for the amount of times it was asked to run.
  */
 const run = () => {
-  ['stateful', 'stateless-functional-mounted', 'stateless-functional-direct-call'].forEach(kind => {
+  ['classicalComponent', 'functionalComponentAsReactElement', 'functionalComponentAsFunction'].forEach(kind => {
     const prevTime = performance.now();
   
     render(
@@ -52,12 +58,12 @@ const run = () => {
       
     const timeToCompletion = Math.round(performance.now() - prevTime);
 
-    if(kind == 'stateless-functional-direct-call') {
-      statelessFunctionalDirectCallTotalTime = statelessFunctionalDirectCallTotalTime + timeToCompletion
-    } else if(kind == 'stateless-functional-mounted') {
-      statelessFunctionalMountedTotalTime = statelessFunctionalMountedTotalTime + timeToCompletion
-    } else if(kind == 'stateful') {
-      statefulTotalTime = statefulTotalTime + timeToCompletion
+    if(kind == 'functionalComponentAsFunction') {
+      functionallyCalledComponent = functionallyCalledComponent + timeToCompletion
+    } else if(kind == 'functionalComponentAsReactElement') {
+      functionalComponentAsElement = functionalComponentAsElement + timeToCompletion
+    } else if(kind == 'classicalComponent') {
+      classicalComponentTotalTime = classicalComponentTotalTime + timeToCompletion
     }
     
     });
@@ -68,12 +74,12 @@ const run = () => {
 //resets the timers on each run
 //calls run as many times as defined
 window.benchmark = ( count = 10 ) => {
-  benchmarkCount = statefulTotalTime = statelessFunctionalMountedTotalTime = statelessFunctionalDirectCallTotalTime = 0;
+  benchmarkCount = classicalComponentTotalTime = functionalComponentAsElement = functionallyCalledComponent = 0;
   console.log(`Running %c${ count } %ctimes ...`, 'font-weight: bold', 'font-weight: normal');
   const countArrayed = new Array(count).fill(0);
   countArrayed.forEach(x => run());
-  console.log(`Class component                        took ${ statefulTotalTime }ms`);
-  console.log(`Functional component mounted           took ${ statelessFunctionalMountedTotalTime }ms %c${Math.round(( 1-statelessFunctionalMountedTotalTime/statefulTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
-  console.log(`Functional invoked component           took ${ statelessFunctionalDirectCallTotalTime }ms %c${Math.round(( 1-statelessFunctionalDirectCallTotalTime/statefulTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
+  console.log(`Class component                        took ${ classicalComponentTotalTime }ms`);
+  console.log(`Functional component mounted           took ${ functionalComponentAsElement }ms %c${Math.round(( 1-functionalComponentAsElement/classicalComponentTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
+  console.log(`Functional invoked component           took ${ functionallyCalledComponent }ms %c${Math.round(( 1-functionallyCalledComponent/classicalComponentTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
   console.log(`%c🎉`, 'font-size: 100px')
 };
