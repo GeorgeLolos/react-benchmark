5fd5b5e039215e793adabcd70834782e81c8cb4e
georgios.lolos <lolos_george@hotmail.com>
2017-06-16 12:30:15 +0200

    beautification

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ import { render } from 'react-dom';
  * @param {Object} props
  * @constructor Dot
  */
-const Dot = (props) =>
-  <span>{props.hi}</span>;
+const ComponentFN = (props) =>
+  <span key={props.index}>{props.hi}</span>;
 
 /**
  * @description Stateful Component
  */
-class DotComponent extends React.Component {
+class Component extends React.Component {
   render() {
     return <span>{this.props.hi}</span>;
   }
@@ -25,35 +25,35 @@ class Main extends React.Component {
   render() {
     //create an array of 500 elements and fill them with zeros
     //Map this array with a function that calls each component in it's way
-    const hi = new Array(500).fill(0).map(x => {
+    const hi = new Array(100).fill(0).map((key, index) => {
       if(this.props.kind == 'functionalComponentAsFunction') {
         //call the functional component as a function
-        return Dot({hi: 'hi'});
+        return ComponentFN({hi: 'hi', index});
       } else if(this.props.kind == 'functionalComponentAsReactElement') {
         //call the functional component as a React Element
-        return <Dot hi="hi"/>;
+        return <ComponentFN key={index} hi="hi"/>;
       } else if(this.props.kind == 'classicalComponent') {
         //call the Class created component as a React Element
-        return <DotComponent hi="hi"/>
+        return <Component key={index} hi="hi"/>
       }
     });
     //create a div and ouput the created elements
-    return React.createElement('div', {}, ...hi);
+    return <div>{hi}</div>
   }
 }
 
 //Benchmarking variables
-let benchmarkCount, classicalComponentTotalTime, functionalComponentAsElement, functionallyCalledComponent;
+let classicalComponentTotalTime, functionalComponentAsElement, functionallyCalledComponent;
 /**
  * @description Run is called as many times as the benchmark defines. Internally it calls 500 times every component for each render.
  * It calculates in total how much time it took to render for the amount of times it was asked to run.
  */
-const run = () => {
+const run = (index) => {
   ['classicalComponent', 'functionalComponentAsReactElement', 'functionalComponentAsFunction'].forEach(kind => {
     const prevTime = performance.now();
   
     render(
-      (<Main kind={kind}/>), document.getElementById(kind)
+      (<Main her={index} kind={kind}/>), document.getElementById(kind)
     );
       
     const timeToCompletion = Math.round(performance.now() - prevTime);
@@ -67,17 +67,16 @@ const run = () => {
     }
     
     });
-    benchmarkCount = benchmarkCount + 1;
-    console.log('.');
+    console.log('Components Rendered ');
 };
 
 //resets the timers on each run
 //calls run as many times as defined
 window.benchmark = ( count = 10 ) => {
-  benchmarkCount = classicalComponentTotalTime = functionalComponentAsElement = functionallyCalledComponent = 0;
+  classicalComponentTotalTime = functionalComponentAsElement = functionallyCalledComponent = 0;
   console.log(`Running %c${ count } %ctimes ...`, 'font-weight: bold', 'font-weight: normal');
   const countArrayed = new Array(count).fill(0);
-  countArrayed.forEach(x => run());
+  countArrayed.forEach((key, index) => run(index));
   console.log(`Class component                        took ${ classicalComponentTotalTime }ms`);
   console.log(`Functional component mounted           took ${ functionalComponentAsElement }ms %c${Math.round(( 1-functionalComponentAsElement/classicalComponentTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
   console.log(`Functional invoked component           took ${ functionallyCalledComponent }ms %c${Math.round(( 1-functionallyCalledComponent/classicalComponentTotalTime) * 100 ) }% %c`, 'color:green', 'color:black');
